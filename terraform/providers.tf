9fde24fccaaea8f1b1a4957f40a90309e4b91ac4
BhupeshDahiya <bhupesh40rajput@gmail.com>
2026-08-14 23:41:42 +0530

    added s3 remote state locking

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,10 +1,17 @@
 terraform {
-  required_version = ">= 1.4"
+  required_version = ">= 1.14"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "6.59.0"
     }
+  }
+  backend "s3" {
+    bucket = "backend-buck-tf"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile =true
+    encrypt = true
   }
 }
 
