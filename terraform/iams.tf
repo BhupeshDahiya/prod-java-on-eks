03530c55f3dcc20af97714de02bb2202fa46132b
BhupeshDahiya <bhupesh40rajput@gmail.com>
2026-09-20 19:23:33 +0530

    changed the pipeline to a dedicated dev env pipeline and updated iams.tf for OIDC for this repo

--- terraform/iams.tf
+++ terraform/iams.tf
@@ -403,7 +403,10 @@ resource "aws_iam_role" "github_actions" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:BhupeshDahiya/Demo_Java_app:*"
+          "token.actions.githubusercontent.com:sub" = [
+            "repo:BhupeshDahiya/Demo_Java_app:*",
+            "repo:BhupeshDahiya/prod-java-on-eks:*"
+          ]
         }
       }
     }]
